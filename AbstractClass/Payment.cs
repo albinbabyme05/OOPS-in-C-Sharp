e101bbd8b5c893c39e82ff6b48452df4bb8a9a29
Albin Baby <albinbabayme05@gmail.com>
2025-04-09 15:11:22 +0200

    WIP: Save local changes before pull

---
+++ AbstractClass/Payment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public abstract class Payment
+    {
+        //abstract method
+        public abstract void ProcessPayment(double amount);
+
+        //non-abstract method
+        public void PrintDetails(double amount)
+        {
+            Console.WriteLine($"Transaction amount: {amount}");
+            Console.WriteLine($"Transaction date: {DateTime.Now}");
+        }
+    }
+}
---
+++ AbstractClass/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+using AbstractClass;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void PrintInterface()
+        {
+            Console.WriteLine("1. Credit Card");
+            Console.WriteLine("2.  Upi payment");
+        }
+        static void Main(string[] args)
+        {
+
+            PrintInterface();
+
+            Console.WriteLine("Select a method: ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Transaction amount in euro: ");
+            double amount = Convert.ToInt32(Console.ReadLine());
+
+            switch (selection)
+            {
+                case 1:
+                    CreditCardPayment creditTrans = new CreditCardPayment();
+                    Console.WriteLine("Credit card tarnsfer initiated...");
+                    creditTrans.ProcessPayment(amount);
+                    break;
+
+                case 2:
+                    UpiPayment upiTrasn = new UpiPayment();
+                    Console.WriteLine("Upi tarnsfer initiated...");
+                    upiTrasn.ProcessPayment(amount);
+                    break;
+
+                default:
+                    Console.WriteLine("invalid selection");
+                    break;
+                    
+            }
+
+
+
+
+
+        }
+    }
+}
+
