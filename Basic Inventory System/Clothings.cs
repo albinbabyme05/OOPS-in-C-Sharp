d2521973e178979691c16fac721a378580a4d204
Albin Baby <albinbabayme05@gmail.com>
2025-04-09 22:38:16 +0200

    Subclass defined for Clothings and inherited from Items class and discountmethod defined

--- Basic Inventory System/Clothings.cs
+++ Basic Inventory System/Clothings.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Basic_Inventory_System
 {
-    internal class Clothings
+    internal class Clothings : Items
     {
+        public  Clothings(string name, decimal price, int quantity) : base(name, price, quantity)
+        {
+
+        }
+
+        public override decimal CalculateDiscountPrice()
+        {
+            decimal off = 0.2m;
+            if (GetQuantity() > 3)
+            {
+                return GetPrice() * GetQuantity() * (1 - off);
+            }
+            return GetPrice() * GetQuantity();
+        }
     }
 }
