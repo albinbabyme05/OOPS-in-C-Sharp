2fcd8bb593f33fe37fa8ddbbf3e81a33cf178508
Albin Baby <albinbabayme05@gmail.com>
2025-04-09 12:19:17 +0200

    calculator example covers abstraction method

---
+++ Calculator with operator/Calcualtor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_with_operator
+{
+    public abstract class Calcualtor
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+
+        //constructor
+        public Calcualtor(double number1, double number2)
+        {
+            Number1 = number1;
+            Number2 = number2;
+        }
+
+        //methods
+
+        public abstract double Addition(double number1, double number2);
+        public abstract double Substraction(double number1, double number2);
+        public abstract double Multiplication(double number1, double number2);
+        public abstract double Division(double number1, double number2);
+
+
+
+    }
+}
---
+++ Calculator with operator/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+namespace Calculator_with_operator
+{
+    class Program
+    {
+        public static void DisplayInfo()
+        {
+            Console.WriteLine("1.Additon \n 2.Subtraction \n 3.Multiplication \n 4.Division ");
+            Console.WriteLine("Enter your choice : ");
+        }
+
+        static void Main(string[] args)
+        {
+
+            DisplayInfo();
+            double choice = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the first number : ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            //create an instance of the object
+            Calcualtor op1 = new Operations(num1, num2);
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine($"Result of {num1} + {num2} : {op1.Addition(num1, num2)}");                   
+                    break;
+
+                case 2:
+                    Console.WriteLine($"Result of {num1} - {num2} : {op1.Substraction(num1, num2)}");
+                    break;
+
+                case 3:
+                    Console.WriteLine($"Result of {num1} - {num2} : {op1.Multiplication(num1, num2)}");
+                    break;
+
+                case 4:
+                    Console.WriteLine($"Result of {num1} - {num2} : {op1.Division(num1, num2)}");
+                    break;
+
+                default:
+                    Console.WriteLine("invalid entry");
+                    break;
+            }
+
+
+
+
+
+        }
+    }
+}
+
