574ee42b97dc18fc6aac3f6342a3d1dcb12f5349
Albin Baby <albinbabayme05@gmail.com>
2025-04-09 22:37:53 +0200

    Subclass defined for Grocery and inherited from Items class and discountmethod defined

--- Basic Inventory System/Grocery.cs
+++ Basic Inventory System/Grocery.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Basic_Inventory_System
 {
-    internal class Grocery
+    internal class Grocery : Items
     {
+        public Grocery(string name, decimal price, int quantity) : base(name, price, quantity)
+        {
+
+        }
+
+        public override decimal CalculateDiscountPrice()
+        {
+            decimal off = 0.05m;
+            
+            return GetPrice() * GetQuantity() * (1 - off);
+            
+        }
     }
 }
