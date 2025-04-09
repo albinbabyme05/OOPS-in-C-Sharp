035f68c8816cc0e3ee16d4984f1f91f940eaf64b
Albin Baby <albinbabayme05@gmail.com>
2025-04-09 22:36:03 +0200

    set property as private and defined setter and getters

--- Basic Inventory System/Items.cs
+++ Basic Inventory System/Items.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace Basic_Inventory_System
 {
-    internal class Items
+    public class Items
     {
+        private string Name { get; set; }
+        private decimal Price { get; set; }
+        private int Quantity { get; set; }
+
+        public Items(string name, decimal price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetPrice(decimal price)
+        {
+            Price = price;
+        }
+        public decimal GetPrice()
+        {
+            return Price;
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            Quantity = quantity;
+        }
+        public int GetQuantity()
+        {
+            return Quantity;
+        }
+
+        public virtual decimal CalculateDiscountPrice()
+        {
+            return Price * Quantity;    
+        }
+        
+
+        
+
     }
 }
