04bec1db4486c534071f1be41de686ac187a4bc2
Albin Baby <albinbabayme05@gmail.com>
2025-04-09 14:28:57 +0200

    Shape clacultor example covers Polymorphism and inheritance

---
+++ ShapeCalculator/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeCalculator
+{
+    public class Shape
+    {
+        public double Area { get; set; }
+
+        public Shape()
+        {
+            Area = 0;
+        }
+
+        public virtual void ResultArea()
+        {
+            Console.WriteLine("Area : " +Area);
+        }
+    }
+}
