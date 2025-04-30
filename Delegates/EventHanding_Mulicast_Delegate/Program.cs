e93d02c6acebc758dc384f83d3e3ee663b61ec24
Albin Baby <albinbabayme05@gmail.com>
2025-04-30 23:30:39 +0200

    delegates with mulicast example

---
+++ Delegates/EventHanding_Mulicast_Delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using EventHanding_Mulicast_Delegate;
+using System.Net.Http.Headers;
+
+namespace EventHanding_Mulicast_Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Video video = new Video("Test Video");
+            EmailService email = new EmailService();
+            SMSService sms = new SMSService();
+
+            // using multicast delegate to point to different methods
+            video.onVideoUploaded += email.SendEmail;
+            video.onVideoUploaded += sms.SendSMS;
+
+            video.UploadVideo();
+
+        }
+    }
+}
