7cc1a8a3c800f4548192dca9822c7ccf8c566590
Albin Baby <albinbabayme05@gmail.com>
2025-05-02 11:33:27 +0200

    delegate examples added

---
+++ Delegates/Discounts/Program.cs
@@ -0,0 +1,26 @@
+﻿using Discounts;
+
+namespace Discounts
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Store s = new Store();
+            Store s1 = new Store();
+            Store s2 = new Store();
+            Discount d = new Discount();
+            s.discount = d.FlatDiscount;
+            s1.discount = d.PercentageDiscount;
+            s2.discount = d.ByoneGetOne;
+
+            double res = s.discount(10, 48.99);
+            Console.WriteLine($"customer 1 : {res}");
+            double res1 = s1.discount(22, 48.99);
+            Console.WriteLine($"customer 2 : {res1}");
+            double res2 = s2.discount(4, 48.99);
+            Console.WriteLine($"customer 3 : {res2}");
+
+        }
+    }
+}
---
+++ Delegates/Temperature_Alter_System/Email.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature_Alter_System
+{
+    public class Email
+    {
+        public void SendEmail(float value)
+        {
+            Console.WriteLine("Sending email...");
+
+            Thread.Sleep(2000);
+            Console.WriteLine("email has sent.");
+        }
+
+    }
+}
